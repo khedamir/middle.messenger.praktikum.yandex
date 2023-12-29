7e363400416c32f1e0f2112ae1e2d723b6e68183
kheda <kheda.amirova@gmail.com>
2023-12-29 14:08:36 +0300

    add project

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+export { AuthForm } from "./auth_form";
+export { Button } from "./button";
+export { Input } from "./input";
+export { Chats } from "./chats";
+export { Search } from "./search";
+export { ChatItem } from "./chat_item";
+export { UserIcon } from "./user_icon";
+export { ChatBlock } from "./chat_block";
+export { ChatHeader } from "./chat_header";
+export { ChatInput } from "./chat_input";
+export { Messages } from "./messages";
+export { Message } from "./message";
+export { UserData } from "./user_data";
+export { DataLayout } from "./data_layout";
+export { PasswordChange } from "./password_change";
---
+++ src/components/password_change/index.ts
@@ -0,0 +1,1 @@
+export { default as PasswordChange } from "./password_change.hbs?raw";
